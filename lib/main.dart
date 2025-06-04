eedf9a8126d91db2def5774ec940ea7508380c0c
Pankaj-Suthar-25 <ps25softwaredeveloper@gmail.com>
2025-06-04 15:51:49 +0530

    Animated Responsive Layout

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ResponsiveLayout(),
+      home: AnimatedResponsiveLayout(
+        items: ['text 1', 'text 2'],
+      ),
     );
   }
 }
@@ -49,6 +51,49 @@ class ResponsiveLayout extends StatelessWidget {
         Text('Text Widget 1'),
         Text('Text Widget 2'),
       ],
+    );
+  }
+}
+
+class AnimatedResponsiveLayout extends StatelessWidget {
+  final List<String> items;
+
+  const AnimatedResponsiveLayout({super.key, required this.items});
+
+  Widget _buildRowLayout() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: items.map((item) => _buildTextWidget(item)).toList(),
+    );
+  }
+
+  Widget _buildColumnLayout() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: items.map((item) => _buildTextWidget(item)).toList(),
+    );
+  }
+
+  Widget _buildTextWidget(String text) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text(
+        text,
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Animated Responsive Layout')),
+      body: AnimatedSwitcher(
+        duration: const Duration(milliseconds: 900),
+        child: (screenWidth > 600) ? _buildRowLayout() : _buildColumnLayout(),
+      ),
     );
   }
 }
