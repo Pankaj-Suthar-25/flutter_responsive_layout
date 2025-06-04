3f43e1075e384a60f67fad3055b4b7c08062d013
Pankaj-Suthar-25 <ps25softwaredeveloper@gmail.com>
2025-06-04 13:09:04 +0530

    Responsive Layout

--- lib/main.dart
+++ lib/main.dart
@@ -14,18 +14,41 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Responsive Layout'),
-        ),
-        body: const Center(
-          child: Text(
-            'Welcome to Flutter Responsive Layout!',
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 24),
-          ),
-        ),
-      ),
+      home: ResponsiveLayout(),
+    );
+  }
+}
+
+class ResponsiveLayout extends StatelessWidget {
+  const ResponsiveLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Responsive Layout')),
+      body: screenWidth > 600 ? _buildRow() : _buildColumn(),
+    );
+  }
+
+  Widget _buildRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Text('Text Widget 1'),
+        Text('Text Widget 2'),
+      ],
+    );
+  }
+
+  Widget _buildColumn() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Text('Text Widget 1'),
+        Text('Text Widget 2'),
+      ],
     );
   }
 }
