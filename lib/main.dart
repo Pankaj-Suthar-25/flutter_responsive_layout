c8b6cff74321388795591f8d69cae3fda27a6280
Pankaj-Suthar-25 <ps25softwaredeveloper@gmail.com>
2025-06-04 16:46:13 +0530

    Responsive card Layout

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: AnimatedResponsiveLayout(
-        items: ['text 1', 'text 2'],
+      home: ResponsiveCardLayout(
+        items: ['text 1', 'text 2','text 1', 'text 2','text 1', 'text 2','text 1', 'text 2','text 1', 'text 2',],
       ),
     );
   }
@@ -94,6 +94,54 @@ class AnimatedResponsiveLayout extends StatelessWidget {
         duration: const Duration(milliseconds: 900),
         child: (screenWidth > 600) ? _buildRowLayout() : _buildColumnLayout(),
       ),
+    );
+  }
+}
+
+class ResponsiveCardLayout extends StatelessWidget {
+  const ResponsiveCardLayout({super.key, required this.items});
+
+  final List<String> items;
+
+  Widget _buildCard(String text) {
+    return Card(
+      elevation: 4,
+      margin: const EdgeInsets.all(8.0),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          text,
+          style: const TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildRowLayout() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: items.map((item) => _buildCard(item)).toList(),
+      ),
+    );
+  }
+
+  Widget _buildColumnLayout() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Column(
+        children: items.map((item) => _buildCard(item)).toList(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Responsive Card Layout')),
+      body: (screenWidth > 600) ? _buildRowLayout() : _buildColumnLayout(),
     );
   }
 }
